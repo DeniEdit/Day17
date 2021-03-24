0efa3bc29651f0663ee493c6be23cff082e5b782
DeniEdit <milicevic.deni@gmail.com>
2021-03-24 15:26:43 +0100

    Exercise 09

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+const _ = required('lodash');
+
+function getUsers() {
+    getUsers 
+}
+
+function findUserById(id) {
+    try {
+
+    } catch (error) {
+        return 
+        console.log();
+    }
+}
+
+getUsers();
+findUserById();
+module.exports = findUserById;
